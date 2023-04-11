edfe54e9f2096abe3eff8d8866a6e91a714ab548
Harshul Jain <harshul@csu.fullerton.edu>
2023-04-10 17:34:09 -0700

    Final Commit

--- main.py
+++ main.py
@@ -1,152 +1,139 @@
-import random
-import discord.ext
+import discord
+import os
+from discord import embeds
 from discord.ext import commands
+import copy
+from webserver import keep_alive
 
-user_name_list = []
+my_secret = os.environ['DISCORD_BOT_SECRET']
+bot = commands.Bot(command_prefix='#')
 
-client = discord.Client()
-prefix = "#"
-client = commands.Bot(command_prefix=prefix)
-say_name = ''
+timeswithnames = ["6am: ", "7am: ", "8am: ", "9am: ", "10am: ", "11am: ", "12pm: ", "1pm: ", "2pm: ", "3pm: ", "4pm: ", "5pm: ", "6pm: "]
 
-
-@client.command()
-async def info(ctx):
-    await ctx.send('''
-        This is a Fun Bot that is currently in development, the commands that this bot provides so far include:
-        - hello (Registers all new users)
-        - #info (shows all commands and their functions)
-        - #say (repeats what you type after '#say')
-        - #changename (changes the name you registered  with)
-        - #flipacoin (flips a coin)
-        - #chooseone (chooses a name from a list you provide)
-        That's all for now stay tuned for more
-        ''')
+bot.remove_command('help')
 
 
-@client.command()
-async def say(ctx, *, content: str):
-    await ctx.send(content)
+@bot.command()
+async def help(ctx):
+    side = discord.Embed(title='Help',
+                         description='Here here are all the commands aviailable to use!',
+                         colour=discord.Colour.blue())
+    side.add_field(name='#join', value="followed with time including am/pm adds you to the schedule", inline=False)
+    side.add_field(name='#canceljoin', value="removes you from the schedule", inline=False)
+    side.add_field(name='#schedule', value="shows the times with everyone signed up", inline=False)
+    side.add_field(name='#clearschedule', value="reformats the whole schedule for a new day", inline=False)
+    await ctx.send(embed=side)
 
 
-@client.command()
-async def flipacoin(ctx):
-    coin = [1,2]
-    choice = random.choice(coin)
-    await ctx.send("Fliping a quarter...")
-    if choice == 1:
-        await ctx.send("Its Heads!")
-    else:
-        await ctx.send("Its Tails!")
+@bot.command()
+async def clearschedule(ctx):
+    timeswithnames[0] = "6am: "
+    timeswithnames[1] = "7am: "
+    timeswithnames[2] = "8am: "
+    timeswithnames[3] = "9am: "
+    timeswithnames[4] = "10am: "
+    timeswithnames[5] = "11am: "
+    timeswithnames[6] = "12pm: "
+    timeswithnames[7] = "1pm: "
+    timeswithnames[8] = "2pm: "
+    timeswithnames[9] = "3pm: "
+    timeswithnames[10] = "4pm: "
+    timeswithnames[11] = "5pm: "
+    timeswithnames[12] = "6pm: "
+    await ctx.channel.send("The schedule has been reset!")
 
 
-@client.command()
-async def new(ctx):
-    my_file = open("nick_names.txt", "a")
-    user_name = str(ctx.author)
-    if str(user_name) not in userid_name_dic:
-        await ctx.channel.send("What do you want me to call you?")
-
-        def check(m):
-            return m.author.id == ctx.author.id
-
-        message = await client.wait_for('message', check=check)
-        await ctx.send(f'Hello, {message.content}! I will remember you now!')
-        userid_name_dic[user_name] = message.content
-        my_file.write(f'{user_name}:{message.content}\n')
-
-    else:
-        await ctx.channel.send('You are already registered')
-        await ctx.channel.send("If you want to change your name do '#changename'")
-    my_file.close()
-
-
-@client.command()
-async def changename(ctx):
-    user_name = str(ctx.author)
-    await ctx.channel.send("What is your new name?")
-
-    def check(m):
-        return m.author.id == ctx.author.id
-
-    message = await client.wait_for('message', check=check)
-    await ctx.send(f'Hello, {message.content}! I will remember you now!')
-    change_txt(userid_name_dic[user_name], message.content)
-    userid_name_dic[user_name] = message.content
-    nicknames = open('nick_names.txt')
-    nicknames.close()
-
-
-@client.command()
-async def chooseone(ctx):
-    await ctx.channel.send("Give me a list of names separated by a comma")
-
-    def check(m):
-        return m.author.id == ctx.author.id
-
-    message = await client.wait_for('message', check=check)
-    x = message.content.split(',')
-    rand = random.choice(x)
-
-    await ctx.channel.send(f'''You gave me the following names: {x}
-I choose {rand}!''')
+@bot.command()
+async def join(ctx, message=None):
+    user = ctx.author.name
+    if message == "6am":
+        timeswithnames[0] = timeswithnames[0] + ", " + user
+        await ctx.channel.send("Congrats " + user + " you are signed up for 6am!")
+    elif message == "7am":
+        timeswithnames[1] = timeswithnames[1] + ", " + user
+        await ctx.channel.send("Congrats " + user + " you are signed up for 7am!")
+    elif message == "8am":
+        timeswithnames[2] = timeswithnames[2] + ", " + user
+        await ctx.channel.send("Congrats " + user + " you are signed up for 8am!")
+    elif message == "9am":
+        timeswithnames[3] = timeswithnames[3] + ", " + user
+        await ctx.channel.send("Congrats " + user + " you are signed up for 9am!")
+    elif message == "10am":
+        timeswithnames[4] = timeswithnames[4] + ", " + user
+        await ctx.channel.send("Congrats " + user + " you are signed up for 10am!")
+    elif message == "11am":
+        timeswithnames[5] = timeswithnames[5] + ", " + user
+        await ctx.channel.send("Congrats " + user + " you are signed up for 11am!")
+    elif message == "12pm":
+        timeswithnames[6] = timeswithnames[6] + ", " + user
+        await ctx.channel.send("Congrats " + user + " you are signed up for 12pm!")
+    elif message == "1pm":
+        timeswithnames[7] = timeswithnames[7] + ", " + user
+        await ctx.channel.send("Congrats " + user + " you are signed up for 1pm!")
+    elif message == "2pm":
+        timeswithnames[8] = timeswithnames[8] + ", " + user
+        await ctx.channel.send("Congrats " + user + " you are signed up for 2pm!")
+    elif message == "3pm":
+        timeswithnames[9] = timeswithnames[9] + ", " + user
+        await ctx.channel.send("Congrats " + user + " you are signed up for 3pm!")
+    elif message == "4pm":
+        timeswithnames[10] = timeswithnames[10] + ", " + user
+        await ctx.channel.send("Congrats " + user + " you are signed up for 4pm!")
+    elif message == "5pm":
+        timeswithnames[11] = timeswithnames[11] + ", " + user
+        await ctx.channel.send("Congrats " + user + " you are signed up for 5pm!")
+    elif message == "6pm":
+        timeswithnames[12] = timeswithnames[12] + ", " + user
+        await ctx.channel.send("Congrats " + user + " you are signed up for 6pm!")
 
 
-userid_name_dic = {}
+@bot.command()
+async def canceljoin(ctx):
+    user = ctx.author.name
+    for x in range (13):
+        if user in timeswithnames[x]:
+            teststring = timeswithnames[x].replace(", " + user, "")
+            timeswithnames[x] = teststring
+    await ctx.channel.send("You have been removed from the schedule!")
 
 
-@client.command()
-async def read(ctx):
-    nicknames = open('nick_names.txt', 'r')
-    user_code = ''
-    user_name = ''
-    for line in nicknames:
-        user_code_name = line.split(':')
-        user_code = user_code_name[0]
-        user_name = user_code_name[1]
-    await ctx.send(f'the username is {user_code} and the nickname is {user_name}')
+@bot.command()
+async def schedule(ctx):
+    side = discord.Embed(title = 'Schedule',
+                          description= 'Here is the current schedule with all players',
+                          colour= discord.Colour.blue())
+    one = timeswithnames[0]
+    two = timeswithnames[1]
+    three = timeswithnames[2]
+    four = timeswithnames[3]
+    five = timeswithnames[4]
+    six = timeswithnames[5]
+    seven = timeswithnames[6]
+    eight = timeswithnames[7]
+    nine = timeswithnames[8]
+    ten = timeswithnames[9]
+    eleven = timeswithnames[10]
+    twelve = timeswithnames[11]
+    thirteen = timeswithnames[12]
 
 
-@client.event
-async def on_ready():
-    print("Everything's all ready to go~")
+
+    side.add_field(name = '.', value = one, inline = False)
+    side.add_field(name = '.', value = two, inline = False)
+    side.add_field(name = '.', value = three, inline = False)
+    side.add_field(name = '.', value = four, inline = False)
+    side.add_field(name = '.', value = five, inline = False)
+    side.add_field(name = '.', value = six, inline = False)
+    side.add_field(name = '.', value = seven, inline = False)
+    side.add_field(name = '.', value = eight, inline = False)
+    side.add_field(name = '.', value = nine, inline = False)
+    side.add_field(name = '.', value = ten, inline = False)
+    side.add_field(name = '.', value = eleven, inline = False)
+    side.add_field(name = '.', value= twelve, inline=False)
+    side.add_field(name ='.', value= thirteen, inline=False)
 
 
-def read_txt_build_dic():
-    names = open('nick_names.txt', 'r')
+    await ctx.send(embed=side)
 
-    for line in names:
-        user_code_name = line.split(':')
-        userid_name_dic[user_code_name[0]] = user_code_name[1]
-    names.close()
-
-
-def change_txt(previous, newer):
-    fin = open("nick_names.txt", "rt")
-    data = fin.read()
-    data = data.replace(previous, newer)
-    fin.close()
-    fin = open("nick_names.txt", "wt")
-    fin.write(data)
-    fin.close()
-
-
-@client.event
-async def on_message(message):
-    message.content = message.content.lower()
-    user = message.author
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('hello'):
-        read_txt_build_dic()
-        if str(user) in userid_name_dic:
-            await message.channel.send(f'Hello {userid_name_dic.get(str(user))} what do you want?')
-        else:
-            await message.channel.send("""
-                                        Hello there! I haven't seen you before
-                                        please use '#new' command so that I can remember you!
-                                        """)
-    await client.process_commands(message)
-
-client.run('ODUyMzQzOTQ4Njc1NTE0NDE4.YMFdKQ.3DjzB3aye4woytq4q4dLsPMgU7w')
+keep_alive()
+bot.run(my_secret)
